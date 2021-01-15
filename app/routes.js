5796b63518509e2f087f73031d590511d837a43b
Dominic Norris <you@example.com>
2021-01-15 13:54:34 +0000

    New iteration with warning text component

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 // Validation routing depending on sort code that's entered
+router.use('/05', require('./views/05/_routes'));
 router.use('/04', require('./views/04/_routes'));
 router.use('/03', require('./views/03/_routes'));
 router.use('/change-account', require('./views/change-account/_routes'));
---
+++ app/views/05/_routes.js
@@ -0,0 +1,131 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+router.post('/whose-account-is-it', function (req, res) {
+
+  const whoseAccount = req.session.data['whose-account']
+
+  if (whoseAccount === 'yes') {
+    res.redirect('my-name/bank-details')
+  } else {
+    res.redirect('someone-elses/bank-details')
+  }
+})
+
+// Validation routing depending on sort code that's entered
+
+router.post('/someone-elses/someone-else', function (req, res) {
+
+  let sortCode = req.session.data['sort-code']
+
+  if (sortCode === '112233') {
+    res.redirect('account-number-error')
+  }
+
+  else if (sortCode === '1122334') {
+    res.redirect('sort-code-error')
+  }
+
+  else if (sortCode === '111111') {
+    res.redirect('roll-number-error')
+  }
+
+  else if (sortCode === '222222') {
+    res.redirect('sort-code-changed')
+  }
+
+  else if (sortCode === '333333') {
+    res.redirect('sort-code-does-not-exist')
+  }
+
+  else if (sortCode === '444444') {
+    res.redirect('roll-number-format-error')
+  }
+
+  else if (sortCode === '555555') {
+    res.redirect('account-number-does-not-exist')
+  }
+
+  else if (sortCode === '') {
+    res.redirect('enter-sort-code')
+  }
+
+  else if (sortCode === '666666') {
+    res.redirect('interruption-success')
+  }
+
+  else {
+    res.redirect('check-answers')
+  }
+})
+
+router.post('/my-name/change-my-name', function (req, res, next) {
+
+  let sortCode2 = req.session.data['sort-code2']
+
+  if (sortCode2 === '112233') {
+    res.redirect('account-number-error')
+  }
+
+  else if (sortCode2 === '1122334') {
+    res.redirect('sort-code-error')
+  }
+
+  else if (sortCode2 === '111111') {
+    res.redirect('roll-number-error')
+  }
+
+  else if (sortCode2 === '222222') {
+    res.redirect('sort-code-changed')
+  }
+
+  else if (sortCode2 === '333333') {
+    res.redirect('sort-code-does-not-exist')
+  }
+
+  else if (sortCode2 === '444444') {
+    res.redirect('roll-number-format-error')
+  }
+
+  else if (sortCode2 === '555555') {
+    res.redirect('account-number-does-not-exist')
+  }
+
+  else if (sortCode2 === '666666') {
+    res.redirect('interruption-success')
+  }
+
+  else if (sortCode2 === '') {
+    res.redirect('enter-sort-code')
+  }
+
+  else {
+    res.redirect('my-name/check-answers')
+  }
+})
+
+router.post('/my-name/change-interruption', function (req, res) {
+
+  const changeInterruption = req.session.data['interruption']
+
+  if (changeInterruption === 'yes') {
+    res.redirect('bank-details')
+  } else {
+    res.redirect('continue-app')
+  }
+})
+
+router.post('/someone-elses/change-interruption', function (req, res) {
+
+  const changeInterruption = req.session.data['interruption']
+
+  if (changeInterruption === 'yes') {
+    res.redirect('bank-details')
+  } else {
+    res.redirect('continue-app')
+  }
+})
+
+module.exports = router
