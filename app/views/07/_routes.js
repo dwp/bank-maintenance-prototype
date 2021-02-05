1b6f501a9ad5a95b43a6680585d355779927fd13
dommn <47447454+dommn@users.noreply.github.com>
2021-02-05 10:27:58 +0000

    Added routing for staff-03

--- app/views/07/_routes.js
+++ app/views/07/_routes.js
@@ -107,4 +107,49 @@ router.post('/staff-02/someone-else', function (req, res) {
   }
 })
 
+router.post('/staff-03/someone-else', function (req, res) {
+
+  let sortCode = req.session.data['sort-code']
+
+  if (sortCode === '112233') {
+    res.redirect('account-number-error')
+  }
+
+  else if (sortCode === '1122334') {
+    res.redirect('sort-code-error')
+  }
+
+  else if (sortCode === '111111') {
+    res.redirect('roll-number-error')
+  }
+
+  else if (sortCode === '222222') {
+    res.redirect('sort-code-changed')
+  }
+
+  else if (sortCode === '333333') {
+    res.redirect('sort-code-does-not-exist')
+  }
+
+  else if (sortCode === '444444') {
+    res.redirect('roll-number-format-error')
+  }
+
+  else if (sortCode === '555555') {
+    res.redirect('account-number-does-not-match')
+  }
+
+  else if (sortCode === '') {
+    res.redirect('enter-sort-code')
+  }
+
+  else if (sortCode === '666666') {
+    res.redirect('interruption-success')
+  }
+
+  else {
+    res.redirect('check-answers')
+  }
+})
+
 module.exports = router
